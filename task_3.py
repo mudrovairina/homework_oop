77788d4db5cd2b8ddb74f7bab8de788114d4761e
Ira Mudrova <mudrovairina18@yandex.ru>
2023-11-25 13:18:17 +0400

    Polymorphism and magic methods

---
+++ task_3.py
@@ -0,0 +1,73 @@
+class AvgGrade:
+    def avg_grade(self):
+        for course, grades in self.grades.items():
+            return sum(grades)/len(grades)
+
+    def __ge__(self, other):
+        return self.avg_grade() >= other.avg_grade()
+
+    def __lt__(self, other):
+        return self.avg_grade() < other.avg_grade()
+
+
+class Student(AvgGrade):
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)
+
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return f'Имя: {self.name}\n' \
+               f'Фамилия: {self.surname}\n' \
+               f'Средняя оценка за лекции: {self.avg_grade()}\n' \
+               f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n' \
+               f'Завершенные курсы: {", ".join(self.finished_courses)}'
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(AvgGrade, Mentor):
+    def __init__(self, name, surname):
+        self.grades = {}
+        super().__init__(name, surname)
+
+    def __str__(self):
+        return f'Имя: {self.name}\n' \
+               f'Фамилия: {self.surname}\n' \
+               f'Средняя оценка за лекции: {self.avg_grade()}'
+
+
+class Reviewer(Mentor):
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return f'Имя: {self.name}\n' \
+               f'Фамилия: {self.surname}'
